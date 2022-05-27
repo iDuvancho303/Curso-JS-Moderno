a9a130e23bbd5fcd59e438dfb2620fbf7b14f810
duvancho303 <duvancho303@gmail.com>
2022-05-26 22:28:15 -0600

    adding more code

--- 01-Introduccion/js/script.js
+++ 01-Introduccion/js/script.js
@@ -1,3 +1,8 @@
+// cree una variable con el nombre "nombre y le pregunta a el usuario cual es su nombre"
 const nombre = prompt('Cual es tu nombre muchacho?');
 
+// Añadi texto  a la "clase" .contenido y despues asigne la variable
 document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JS moderno`;
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monito de 24 pulgadas';
