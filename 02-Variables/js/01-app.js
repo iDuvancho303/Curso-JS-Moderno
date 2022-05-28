4f54ad1f7b162bdf9956056f0c68b0d2a0bdf0fa
duvancho303 <duvancho303@gmail.com>
2022-05-27 22:06:24 -0600

    adding more code

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,11 @@
+// variables
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+// re asignar o cambiar valor de la variable anterior
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+// JavaScript es una lenguaje de tipo dinamico, no se expecifica tipo de dato
+producto = 20;
+console.log(producto);
