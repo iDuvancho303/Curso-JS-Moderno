dc6541215b75df6d8aa60e38ccc8759db50e2a19
duvancho303 <duvancho303@gmail.com>
2022-05-27 22:20:18 -0600

    adding more code

--- 02-Variables/js/01-app.js
+++ 02-Variables/js/01-app.js
@@ -9,3 +9,21 @@ console.log(producto);
 // JavaScript es una lenguaje de tipo dinamico, no se expecifica tipo de dato
 producto = 20;
 console.log(producto);
+
+// las variables tambien se pueden inicializar sin valor y asignarlo despues
+var disponible;
+
+disponible = true;
+
+disponible = false;
+
+// inicializar multiples variables usando comas
+var categoria = "computadoras",
+    marca = "Marca Famosa";
+    calificacion = 5;
+
+// las variables no pueden empezar con numeros
+ // var 99dias; // esta mal
+
+  // var 01_; // esta mal
+
