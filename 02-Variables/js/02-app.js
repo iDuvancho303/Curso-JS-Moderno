eac9cd6c0d96664f6a9d49fb4afeb31bc2bd44e7
duvancho303 <duvancho303@gmail.com>
2022-05-27 22:26:45 -0600

    adding more code

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,17 @@
+// variable "let"
+let producto = 'Tablet'; 
+
+// se puede reasignar el valor 
+producto = 'Monitor';
+
+producto = 20;
+
+producto = true;
+
+producto = null; 
+
+console.log(producto);
+
+
+
+
