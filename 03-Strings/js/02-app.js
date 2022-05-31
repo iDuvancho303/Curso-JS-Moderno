42eaad2b71dafb325dd66a31ecdda1f11a3641f3
duvancho303 <duvancho303@gmail.com>
2022-05-31 17:49:02 -0600

    adding more code

--- 03-Strings/js/02-app.js
+++ 03-Strings/js/02-app.js
@@ -7,6 +7,14 @@ console.log(producto.length);  // me devuelve = 24
 // usando el "Method" de .indexOf - sirve para buscar una letra o palabra en especifico
 console.log(producto.indexOf('de')); 
 // si el string "producto" tiene la palabrar "de" en algun lugar, nos dira la posicion
-// en este lugar serila en la posicion #9
+// en este lugar serila en la posicion, en la letra numero 9
+
+
+// usando el "method" de .includes - que es lo mismo que .indexOf
+// sirven igual, cual se usara mas ?
+console.log(producto.includes('pulgadas'));
+// en este caso te devuelve "true"
+
+
 
 
