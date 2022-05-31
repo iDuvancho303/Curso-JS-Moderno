1a827f9b4fe5521349354c05e620fa4bb1e73d8c
duvancho303 <duvancho303@gmail.com>
2022-05-31 17:56:05 -0600

    adding more code

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,12 @@
+//concatenar un string y template strings
+const producto = "telefono de 20 pulgadas ";
+const precio = '30 USD'
+
+console.log(producto.concat(precio));
+// me devuelve "telefono de 20 pulgadas"
+
+// tambien puedes hacer esto
+// concatenar un string
+console.log(producto.concat('esta en rebaja'));
+// me devuelve "telefono de 20 pulgadas esta en rebaja"
+
