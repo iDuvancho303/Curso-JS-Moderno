e92a5822ce499a1c8eaf783c4ce4f1e1f8d31428
duvancho303 <duvancho303@gmail.com>
2022-05-31 17:43:12 -0600

    adding more code

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,12 @@
+// este es un string usando el "Method" de .length
+const producto = "telefono de 20 pulgadas";
+
+console.log(producto);         // me devuelve = "telefono de 20 pulgadas"
+console.log(producto.length);  // me devuelve = 24 
+
+// usando el "Method" de .indexOf - sirve para buscar una letra o palabra en especifico
+console.log(producto.indexOf('de')); 
+// si el string "producto" tiene la palabrar "de" en algun lugar, nos dira la posicion
+// en este lugar serila en la posicion #9
+
+
