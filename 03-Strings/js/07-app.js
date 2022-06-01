6ee1f7c84fa6ab37106bc9a8d4560d0097201eb3
duvancho303 <duvancho303@gmail.com>
2022-05-31 21:12:03 -0600

    adding more code

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,8 @@
+
+const producto = "telefono de 20 pulgadas";
+
+console.log(producto.toUpperCase());
+// me devuelve todo el texto en mayusculas
+
+console.log(producto.toLowerCase());
+// me devuelve todo el texto en minisculas
