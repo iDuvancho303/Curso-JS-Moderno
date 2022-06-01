d5cae72b23d80d3d2c2487dae6f1f8296ca2523f
duvancho303 <duvancho303@gmail.com>
2022-05-31 20:46:51 -0600

    adding more code

--- 03-Strings/js/06-app.js
+++ 03-Strings/js/06-app.js
@@ -3,10 +3,23 @@ const producto = "telefono de 20 pulgadas";
 
 
 // usando el method .repeat
-const texto = 'En promocion'.repeat(4);
+const texto = 'En promocion'.repeat();
 
 console.log(texto);
 // me devuelve "en promocion en promocion en promocion en promocion"
+
+// tambien se puede hacer esto
+console.log(`${producto} ${texto}`);
+// me devuelve "telefono de 20 pulgadas en promocion en promocion en promocion"
+
+
+//.split se usa para dividir un string
+const actividad = "Estoy aprendiendo JS moderno"
+
+console.log(actividad.split(" "));
+// me devuelve "estoy" "aprendiendo" "JS" "moderno"
+
+
 
 
 
