a336e2216d27b8c14597d22aa538a7506798ef72
duvancho303 <duvancho303@gmail.com>
2022-06-01 22:55:59 -0600

    adding more code

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,17 @@
+// crear objectos en Javascript
+const nombre = "monitor de 20 pulgadas";
+const precio = 300;
+const disponible = true;
+
+// un objeto agrupa todo en una sola variable...
+// de este modo no tienes que crear muchas variables
+
+// object literal
+const producto = {
+    nombre: 'monitor de 20 pulgadas',
+    precio: 300,
+    disponible: true,
+}
+
+console.log(producto);
+// asegurate de usar ":" y las "," para anadir mas datos
