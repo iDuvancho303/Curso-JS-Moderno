602b97ecee290391bb9738a0d5f3a454ea7672d0
duvancho303 <duvancho303@gmail.com>
2022-06-01 21:41:59 -0600

    adding more code

--- 04-Numeros/js/05-app.js
+++ 04-Numeros/js/05-app.js
@@ -1,6 +1,12 @@
 // incrementos o decrementos 
 let puntaje = 5;
 
+// incrementando uno a uno
+puntaje++; //5
+
+++puntaje //6
+
+
 
 
 
---
+++ 04-Numeros/js/06-app.js
@@ -0,0 +1,8 @@
+// convertir string en numeros
+const numero1 = "20";
+const numero2 = "20.2";
+const numero3 = "treinta";
+const numero4 = 30.5;
+
+// convertir un strin a numero usando parseint
+console.log(Number.parseInt(numero1))
