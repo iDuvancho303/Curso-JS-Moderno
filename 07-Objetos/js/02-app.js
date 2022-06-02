b18c8da01d1ce0f227b3e589e0a5aac03420b38a
duvancho303 <duvancho303@gmail.com>
2022-06-01 23:40:35 -0600

    adding more code

--- 07-Objetos/js/02-app.js
+++ 07-Objetos/js/02-app.js
@@ -1,1 +1,15 @@
 // como acceder a los valores de un objeto
+const producto = {
+    nombre: 'monitor de 20 pulgadas',
+    precio: 300,
+    disponible: true,
+}
+
+console.log(producto);
+
+// accediendo a un valor en concreto
+console.log(producto.nombre);
+// nos devuelve "monitor de 20 pulgadas"
+
+console.log(producto.disponible);
+// nos devuelve "true"
