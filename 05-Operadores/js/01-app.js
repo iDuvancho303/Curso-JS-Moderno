403a3bd0f5dcd8d32c6186c0d510a8adb678b598
duvancho303 <duvancho303@gmail.com>
2022-06-01 21:56:34 -0600

    adding more code

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,17 @@
+// operador mayor que y menor que
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = 30;
+
+
+// operador mayor a...
+console.log(numero1 > numero3);
+// nos devuelve "false" porque 20 no es mayor que 30
+console.log(numero3 > numero1);
+// nos devuelve "true" porque 30 si es mayor que 20
+
+// operador menor a...
+console.log(numero1 < numero3);
+// nos devuelve "true" porque 20 si es menor que 30
+console.log(numero3 < numero1);
+// nos devuelve "false" porque 30 no es menor que 20
