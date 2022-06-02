bedd3fa6ffffeb7447d5ff7a0f8bb0fd08f76266
duvancho303 <duvancho303@gmail.com>
2022-06-01 21:47:17 -0600

    adding more code

--- 04-Numeros/js/06-app.js
+++ 04-Numeros/js/06-app.js
@@ -4,5 +4,20 @@ const numero2 = "20.2";
 const numero3 = "treinta";
 const numero4 = 30.5;
 
-// convertir un strin a numero usando parseint
-console.log(Number.parseInt(numero1))
+// convertir un strin a numero usando parseint, parseint connvierte a numero entero
+console.log(Number.parseInt(numero1)); // convertido a un numero
+console.log(numero1); // sigue siendo un string
+
+// parseint convierte a numero entero
+console.log(Number.parseInt(numero2)); 
+// me devuelve 20 me lo convierte a un numero entero
+
+// convertir a numero con decimales usando parefloat
+console.log(Number.parseFloat(numero2))
+// me devuelve 20.2 - un numero con decimales
+
+// revisar si un numero es entero o no
+console.log(Number.isInteger(numero2));
+// me devuelve "false" porque numero2 no es un numero entero
+
+
