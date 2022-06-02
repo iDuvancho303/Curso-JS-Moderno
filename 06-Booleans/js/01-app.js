78939b47b6889a2af5b094eadd47c054c2100c57
duvancho303 <duvancho303@gmail.com>
2022-06-01 22:31:08 -0600

    adding more code

---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,10 @@
+// crear y comparar "booleans"
+// los booleans solo pueden tener 2 valores ya sea true or false
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(boolean1);
+// nos devuelve "true"
+console.log(boolean2);
+// nos devuelve "false"
+
