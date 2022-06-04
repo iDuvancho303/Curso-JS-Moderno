90044a8f12cb8675b24231d49c07bfa397ac9197
duvancho303 <duvancho303@gmail.com>
2022-06-03 20:19:13 -0600

    adding more code

---
+++ 07-Objetos/js/04-app.js
@@ -0,0 +1,17 @@
+const producto = {
+    nombre: 'monitor de 20 pulgadas',
+    precio: 300,
+    disponible: true,
+}
+
+console.log(producto.nombre);
+
+// destructuring
+const {nombre, disponible} = producto;
+const {precio} = producto;
+console.log(nombre, precio);
+console.log(precio);
+
+// no entendi mucho esto la verdad !!!!!!!!!!!!!!!!!!!!
+
+
