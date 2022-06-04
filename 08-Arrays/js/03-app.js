f8c3e5386a3218d775d9109e78688a3718c33b5b
duvancho303 <duvancho303@gmail.com>
2022-06-04 17:14:09 -0600

    Adding more code

---
+++ 08-Arrays/js/03-app.js
@@ -0,0 +1,12 @@
+// recorrer un array
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Junio', 'Julio']
+
+console.table(meses);
+
+// cuanto mide un arreglo
+console.log(meses.length);
+// nos devuelve 6
+
+for(let i = 0; i < meses.length; i++) {
+    console.log(meses[i]);
+}
