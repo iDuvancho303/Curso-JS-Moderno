045d32524091be72c937c1ce2b0f4fac1baee6c5
duvancho303 <duvancho303@gmail.com>
2022-06-04 09:35:45 -0600

    adding more code

--- 08-Arrays/js/01-app.js
+++ 08-Arrays/js/01-app.js
@@ -1,4 +1,18 @@
 //crear arrays en Javascript
+
 // los arrays se crean con corchetas en vez de llaves como en el los objetos
 // los objetos tienen siempre una llave y un valor en cambio los arreglos no
 const numeros = [10, 20, 30];
+
+// otra manera de crear un array
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+
+console.log(numeros);
+console.log(meses);
+
+
+// un arreglo que contiene datos de todo tipo
+const deTodo = ['hola', 10, true, 'si', null, {nombre: 'duvan', trabajo: 'programador'}];
+
+console.log(deTodo);
+
