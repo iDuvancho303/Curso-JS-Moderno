08162df49da6efdf87cce29e0f4c9f1fa9fa870f
duvancho303 <duvancho303@gmail.com>
2022-06-04 07:35:02 -0600

    adding more code

--- 07-Objetos/js/09-app.js
+++ 07-Objetos/js/09-app.js
@@ -1,4 +1,6 @@
-// congelar un objeto para que no se pueda modificar
+// sellar un objeto usando (.seal)para que no se puedan agregar o eliminar propiedades
+// pero si se pueden modificar las existentes
+
 "use strict" // estudiar mas sobre esto
 
 const producto = {
@@ -7,13 +9,15 @@ const producto = {
     disponible: true,
 }
 
-// congela lo que es el objeto
-// en este caso "disponible" sigue siendo "true" aunque yo lo cambie a "false"
-Object.freeze(producto);  // aqui solo aprendi como usar este objeto
+// sella un objeto
+// en este caso "disponible" si puede pasar de "true" a "false"
+Object.seal(producto);  // aqui solo aprendi como usar este objeto
 
 producto.disponible = false;
-producto.imagen = "imagen.jpg";
+// producto.imagen = "imagen.jpg"; <- este copdigo no sirve porque no se puede anadir 
 
 console.log(producto);
 
-console.log(Object.isFrozen(producto));
+// mirar si un objeto esta sellado o no
+console.log(Object.isSealed(producto));
+
