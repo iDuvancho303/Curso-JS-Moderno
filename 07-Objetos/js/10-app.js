2633b232fdcb683cf7f815715ba043760fe600b1
duvancho303 <duvancho303@gmail.com>
2022-06-04 07:39:29 -0600

    adding more code

--- 07-Objetos/js/10-app.js
+++ 07-Objetos/js/10-app.js
@@ -1,6 +1,16 @@
-
+// copiar, unir dos objetos dentro de uno solo
 const producto = {
     nombre: "Monitor de 20 pulgadas",
     precio: 300,
     disponible: true,
 }
+
+const medidas = {
+    peso: '20kg',
+    medida: '1m',
+}
+
+
+const resultado = {...producto, ...medidas}
+
+console.log(resultado);
