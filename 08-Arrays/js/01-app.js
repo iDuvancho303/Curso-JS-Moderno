ada5559f8160fa2478839167c177717730f8cfe7
duvancho303 <duvancho303@gmail.com>
2022-06-04 08:48:02 -0600

    adding code

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,4 @@
+//crear arrays en Javascript
+// los arrays se crean con corchetas en vez de llaves como en el los objetos
+// los objetos tienen siempre una llave y un valor en cambio los arreglos no
+const numeros = [10, 20, 30];
