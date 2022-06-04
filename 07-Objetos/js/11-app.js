ae8bdaf76083989702bf8c24940aee0862ae1387
duvancho303 <duvancho303@gmail.com>
2022-06-04 07:51:08 -0600

    Adding more code

--- 07-Objetos/js/11-app.js
+++ 07-Objetos/js/11-app.js
@@ -1,5 +1,14 @@
+// funciones en objetos y acceder a sus valores
 const producto = {
     nombre: "Monitor de 20 pulgadas",
     precio: 300,
     disponible: true,
+    mostraInfo: function() {
+        console.log(`El producto: ${this.nombre} tiene un precio de ${this.precio}`)
+    }
 }
+
+producto.mostraInfo();
+
+//aqui aprendimos sobre (this.) para selecionar una llave en especifico dentro 
+//de el objeto
