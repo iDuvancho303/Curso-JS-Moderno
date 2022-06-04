5fcee5339a1948a3065d2e50c87d04637405e34a
duvancho303 <duvancho303@gmail.com>
2022-06-04 08:39:48 -0600

    adding more code

--- 07-Objetos/js/13-app.js
+++ 07-Objetos/js/13-app.js
@@ -5,5 +5,14 @@ const producto = {
     disponible: true,
 }
 
-// usando keys para saber si es objeto tiene informacion o no
-console.log(Object.keys(producto))
+// usando (.keys) para saber si el objeto tiene informacion o no
+// me devuelve las llaves que son:  nombre, precio y disponible
+console.log(Object.keys(producto));
+
+// usando (.values) para saber si el objeto tiene valores asignado
+// me devuelve monitor de 20 pulgadas, 300 y true
+console.log(Object.values(producto));
+
+// usando (.entries) esta me retorna todo las llaves y los valores, todo junto
+// me devueve
+console.log(Object.entries(producto));
