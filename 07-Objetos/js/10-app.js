c709a81f9905b4591425fa410614b13ef98ab0b5
duvancho303 <duvancho303@gmail.com>
2022-06-04 07:43:27 -0600

    Adding more code

--- 07-Objetos/js/10-app.js
+++ 07-Objetos/js/10-app.js
@@ -14,3 +14,6 @@ const medidas = {
 const resultado = {...producto, ...medidas}
 
 console.log(resultado);
+
+// me devuelve lo siguente, los dos objetos en uno solo
+// Object { nombre: "Monitor de 20 pulgadas", precio: 300, disponible: true, peso: "20kg", medida: "1m" }
