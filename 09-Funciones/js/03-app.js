4a722558b755444c62f8ada409da92f09c9945a6
duvancho303 <duvancho303@gmail.com>
2022-06-05 08:24:58 -0600

    adding more code

---
+++ 09-Funciones/js/03-app.js
@@ -0,0 +1,7 @@
+// algunas funciones  nativas en JavaScript
+//ejemplos - todos estos tienen la estructura de una function 
+// tiene el nombre de la funcion y el parentesis
+
+alert('Hubo un error...');
+
+prompt('Cual es tu edad?');
