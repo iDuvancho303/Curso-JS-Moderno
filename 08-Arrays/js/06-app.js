07946220439d1a3a843c5c8fca32d7e97b96a894
duvancho303 <duvancho303@gmail.com>
2022-06-04 20:07:00 -0600

    adding more code

---
+++ 08-Arrays/js/06-app.js
@@ -0,0 +1,21 @@
+// crear un nuevo arreglo con el spread operator
+
+const carrito = [];
+
+// definir un producto
+const producto1 = {
+    nombre: 'tablet de 12 pulgadas',
+    precio: 300,
+}
+
+const producto2 = {
+    nombre: 'Telefono de 11 pulgadas',
+    precio: 700,
+}
+
+const producto3 = {
+    nombre: 'Teclado',
+    Precio: 100,
+}
+
+console.table(carrito);
