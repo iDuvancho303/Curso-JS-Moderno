91d55f16fa90a8dcb00876a5bb4115dbde484aaf
duvancho303 <duvancho303@gmail.com>
2022-06-05 08:15:24 -0600

    adding more code

--- 09-Funciones/js/02-app.js
+++ 09-Funciones/js/02-app.js
@@ -1,1 +1,29 @@
 // la diferencia entre Function Expression y Declaration
+
+// en "function declaration" se puede llamar la funcion antes
+// de la funcion
+
+
+//function declaration
+
+sumar();
+
+function sumar() {
+    console.log(2 + 2);
+}
+
+// me devuelve 4 con function declaration
+
+
+
+// function expression
+
+sumar2();
+
+const sumar2 = function() {
+    console.log(3 + 3);
+}
+
+// no me devuelve nada con function expression
+
+
