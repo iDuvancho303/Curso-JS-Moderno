5b3b0fa41daf59748958081320f62021b15618ad
duvancho303 <duvancho303@gmail.com>
2022-06-05 21:10:53 -0600

    adding more code

--- 09-Funciones/js/07-app.js
+++ 09-Funciones/js/07-app.js
@@ -1,1 +1,20 @@
 // como se comunican las funciones
+iniciarApp();
+
+function iniciarApp() {
+    console.log('Iniciando app...');
+
+    segundaFuncion();
+}
+
+function segundaFuncion() {
+    console.log('Iniciando desde la segunda funcion...');
+
+    usuarioAutenticado('Pablo');
+}
+
+
+function usuarioAutenticado(usuario) {
+    console.log('Autenticando usuario');
+    console.log(`Usuario autenticado exitosamente: ${usuario}`);
+}
