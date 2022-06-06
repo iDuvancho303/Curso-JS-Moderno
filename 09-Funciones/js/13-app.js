336fc51110106378af6cfaf88535750ac4921d4a
duvancho303 <duvancho303@gmail.com>
2022-06-06 15:11:27 -0600

    adding more code

--- 09-Funciones/js/13-app.js
+++ 09-Funciones/js/13-app.js
@@ -1,20 +1,21 @@
 // Arrow functions en el reproductor de musica
+//estamos borrando los parenthesis  cuando solo hay un parametro
+//estamos borrando los brackets cuando solo hay una linea de codigo
+
 const reproductor = {
-    reproducir: function(id) {
-        console.log(`Reproduciendo cancion con el id ${id}`)
-    },
-    pausar: function() {
-        console.log('pausando...');
-    },
-    borrar: function(id) {
-        console.log(`Borrando cancion...${id}`)
-    },
-    crearPlaylist: function(nombre) {
-        console.log(`Creadon un playlist de ${nombre}`)
-    }
-}
+    reproducir: id => 
+        console.log(`Reproduciendo cancion con el id ${id}`),
+    
+        pausar: () => console.log('pausando...'),
+    
+    
+        borrar: id => console.log(`Borrando cancion...${id}`),
+    
+        crearPlaylist: nombre => console.log(`Creadon un playlist de ${nombre}`),
+};
 
 reproductor.reproducir(30);
 reproductor.pausar();
 reproductor.borrar(30)
 reproductor.crearPlaylist('Mana');
+
