5203bdd614f961166c1d5b1fe8dd0b253e4ffa26
duvancho303 <duvancho303@gmail.com>
2022-06-06 14:24:19 -0600

    adding more code

--- 09-Funciones/js/12-app.js
+++ 09-Funciones/js/12-app.js
@@ -1,1 +1,10 @@
 // Arrow function en un ForEach y un .map
+const carrito = [
+    {nombre: 'Tablet', precio: 500},
+    {nombre: 'cellphone', precio: 900},
+    {nombre: 'keyboard', precio: 100},
+    {nombre: 'mouse', precio: 90},
+    {nombre: 'headphones', precio: 250},
+]
+
+const nueboArreglo = carrito.map 
