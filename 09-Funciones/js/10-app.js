2eb1a934f9a50acee4ad2bddfde3b3cabfdd5f4d
duvancho303 <duvancho303@gmail.com>
2022-06-05 22:26:11 -0600

    adding more code

---
+++ 09-Funciones/js/10-app.js
@@ -0,0 +1,13 @@
+//arrow functions
+// otra forma de escribir funciones
+// la sintasis es de la siguiente manera
+const aprendiendo = function() {
+    console.log('aprendiendo JS');
+}
+
+//  tranformar a arrow function
+const aprendiendo2 = () => {
+    console.log('Aprendiendo JS');
+}
+
+aprendiendo2();
---
+++ 09-Funciones/js/11-app.js
@@ -0,0 +1,13 @@
+// las ventajas de los arrow functions
+const aprendiendo = function(tecnologia) {
+    console.log(`Aprendiendo ${tecnologia}`);
+}
+
+aprendiendo('JavaScript');
+
+//con arrow function
+const aprendiendo2 = (tecnologia) => {
+    console.log(`Aprendiendo mas con ${tecnologia}`)
+}
+
+aprendiendo2('Python');
