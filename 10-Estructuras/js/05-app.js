44809944870fb92b8e28e274715aa66e68df062f
duvancho303 <duvancho303@gmail.com>
2022-06-06 22:14:54 -0600

    adding more code

--- 10-Estructuras/js/05-app.js
+++ 10-Estructuras/js/05-app.js
@@ -1,1 +1,28 @@
 // usando "switch" para evaluar multiples condiciones
+
+const metodoPago = 'efectivo';
+
+switch(metodoPago) {
+    case 'efectivo':
+        console.log(`Pagaste con ${metodoPago}`);
+        break;
+    default:
+        console.log('Aun no has selecionado un metodo de pago');
+        break;
+}
+
+// nos devuelve "Pagaste con efectivo"
+
+const metodoPago2 = 'cheque';
+
+switch(metodoPago2) {
+    case 'efectivo':
+        console.log('Pagaste con efectivo');
+        break;
+    default:
+        console.log('Metodo de pago no reconocido');
+        break;
+}
+
+// nos devuelve "metodo de pago no reconocido"
+// porque la primera condicion no se cumplio
