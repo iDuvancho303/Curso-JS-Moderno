cb252f2d813c685171e83c861ff09b9bb34155db
duvancho303 <duvancho303@gmail.com>
2022-06-06 22:46:17 -0600

    Adding more code

--- 10-Estructuras/js/07-app.js
+++ 10-Estructuras/js/07-app.js
@@ -1,2 +1,14 @@
 // el operador OR para que cumpla almenos una condicion
 
+const efectivo = 500;
+const credito = 1000;
+const disponible = efectivo + credito;
+const totalPagar = 600
+
+if( efectivo > totalPagar || disponible > totalPagar) {
+    console.log('Si tienes suficiente dinero');
+} else {
+    console.log('fondos insuficientes');
+}
+
+// nos devuelve "Si tienes suficiente dinero"
