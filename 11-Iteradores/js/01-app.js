46090058fc57a08740828469bf1e7f1d17124e71
duvancho303 <duvancho303@gmail.com>
2022-06-07 07:26:47 -0600

    adding more code

--- 11-Iteradores/js/01-app.js
+++ 11-Iteradores/js/01-app.js
@@ -5,3 +5,16 @@
 for(let i = 0; i < 10; i++) {
     console.log(`numero: ${i}`);
 }
+
+// como sacar numeros que son pares ? 
+// ejemplo de como usar un loop
+
+for(let i = 0; i <= 30; i++) {
+    if(i % 2 === 0){
+        console.log(`El ${i} es un numero par`);
+    }
+
+    if(i % 2 === 1){
+        console.log(`el ${i} es un numero impar`)
+    }
+}
