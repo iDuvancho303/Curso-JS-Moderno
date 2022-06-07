8c85c1c389b5650cec941a775013b2e9cd1fc3a4
duvancho303 <duvancho303@gmail.com>
2022-06-06 21:01:23 -0600

    adding more code

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,24 @@
+// comparando mayor o menor que
+
+// mayor que
+const dinero = 500;
+const totalPagar = 300;
+
+if(dinero > totalPagar) {
+    console.log('Si podemos pagar...');
+} else {
+    console.log('No tienes suficiente dinero amigo...');
+}
+// nos devuelve si podemos pagar
+
+// menor que
+const dinero2 = 100;
+const totalPagar2 = 300;
+
+if(dinero2 < totalPagar2) {
+    console.log('No tienes suficiente dinero');
+} else {
+    console.log('Si tienes dinero suficiente');
+}
+// no tienes suficiente dinero
+
