2627af9182ff2451ac52d18a58e8a1339f21545f
duvancho303 <duvancho303@gmail.com>
2022-06-07 19:37:14 -0600

    adding more code

--- 11-Iteradores/js/02-app.js
+++ 11-Iteradores/js/02-app.js
@@ -13,12 +13,17 @@ for(let i = 0; i <= 10; i++) {
 const carrito = [
     { nombre: 'Monitor', precio: 500},
     { nombre: 'Television', precio: 1000},
-    { nombre: 'Tablet', precio: 450},
+    { nombre: 'Tablet', precio: 450, descuento: true},
     { nombre: 'Audifonos', precio: 150},
     { nombre: 'Teclado', precio: 50},
     { nombre: 'Celular', precio: 1200}
 ]
 
 for(let i = 0; i < carrito.length; i++) {
-    
+    if(carrito[i].descuento) {
+        console.log(`El articulo ${carrito[i].nombre} esta en descuento`);
+        continue
+    }
+
+    console.log(carrito[i].nombre);
 }
