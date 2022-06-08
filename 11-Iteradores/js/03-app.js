86fca91487a9115eb4876ca6858c2ebeac3e36a7
duvancho303 <duvancho303@gmail.com>
2022-06-07 21:16:33 -0600

    adding more code

--- 11-Iteradores/js/03-app.js
+++ 11-Iteradores/js/03-app.js
@@ -6,9 +6,14 @@
 // multiples de 3 y 5 .... imprime fizzbuz
 
 for(let i = 1; i <= 100; i++) {
-    if(i % 3 === 0) {
+    if(i % 15 === 0) {
+        console.log(`${i} Fizz Buzz !!!`);
+    } else if(i % 3 === 0) {
         console.log(`${i} Fizz`);
     } else if(i % 5 === 0) {
         console.log(`${i} Buzz`);
     }
 }
+
+// tener en cuentas que hay muchisimas manera de hacer un fizz buzz
+
