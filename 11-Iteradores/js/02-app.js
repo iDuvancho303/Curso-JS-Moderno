530d5e70ac1c6aa51d004fe1938da456175d1971
duvancho303 <duvancho303@gmail.com>
2022-06-07 19:18:46 -0600

    adding more code

---
+++ 11-Iteradores/js/02-app.js
@@ -0,0 +1,24 @@
+// "break" y "continue" en un for loop
+
+for(let i = 0; i <= 10; i++) {
+    if(i === 5) {
+        console.log(`Este es el # ${i}`);
+    }
+
+    console.log(`Numero: ${i}`);
+}
+
+// me devuelve "Este es el # 5"
+
+const carrito = [
+    { nombre: 'Monitor', precio: 500},
+    { nombre: 'Television', precio: 1000},
+    { nombre: 'Tablet', precio: 450},
+    { nombre: 'Audifonos', precio: 150},
+    { nombre: 'Teclado', precio: 50},
+    { nombre: 'Celular', precio: 1200}
+]
+
+for(let i = 0; i < carrito.length; i++) {
+    
+}
