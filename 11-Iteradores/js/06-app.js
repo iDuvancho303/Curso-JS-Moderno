def8b1c848d8587028672a889c4638bae7eeb740
duvancho303 <duvancho303@gmail.com>
2022-06-08 11:21:16 -0600

    adding more code

--- 11-Iteradores/js/06-app.js
+++ 11-Iteradores/js/06-app.js
@@ -1,7 +1,12 @@
 // usando .forEach y .map
+// .forEach es ideal para correr un arreglo
 
 const pendientes = ['Tarea', 'Comer', 'Codigo', 'Estudiar JS'];
 
-pendientes.forEach( (pendiente) => {
-    console.log(pendiente);
-})
+pendientes.forEach( (pendiente, indice) => {
+    console.log(`${indice} : ${pendiente}`);
+}) 
+
+// usando .map
+
+// cual es la diferencia entre .forEach and .map
---
+++ 11-Iteradores/js/07-app.js
@@ -0,0 +1,3 @@
+// usando "for of..."
+
+for
