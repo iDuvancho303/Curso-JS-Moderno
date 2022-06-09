e4d3b216d6035c1d71309842332586a806f08a02
duvancho303 <duvancho303@gmail.com>
2022-06-08 20:54:19 -0600

    adding more code

---
+++ 12-ArrayMethods/js/04-app.js
@@ -0,0 +1,24 @@
+// usando .filter
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+let resultado 
+
+resultado = carrito.filter( producto =>producto.precio > 400);
+
+resultado = carrito.filter( producto => producto.precio < 600 );
+
+resultado = carrito.filter( producto => producto.nombre !== 'Audifonos');
+
+
+console.log(resultado);
+
+// con filter nos crea un nuevo array con la condicion que estas pasando
+// nos devuelve los productos que estan por encima de 400 en este caso son monitor y celular
