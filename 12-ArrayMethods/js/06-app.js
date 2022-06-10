585a6b40e2a1394c8c11725c1dd310ec523c5c70
duvancho303 <duvancho303@gmail.com>
2022-06-09 21:17:46 -0600

    adding more code

--- 12-ArrayMethods/js/06-app.js
+++ 12-ArrayMethods/js/06-app.js
@@ -1,1 +1,18 @@
 // usando .every
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+const resultado = carrito.every( producto => producto.precio < 1000);
+
+console.log(resultado);
+
+// estamos mirando si el precio de todos es menor a 1000
+// nos devuelve "true" porque el precio de todos los productos si es menor a 1000
+// cuando usas every todas las condiciones se tienen que cumplir
