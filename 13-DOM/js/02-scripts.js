59179f2e5714adb630c36f637ef29807db38f405
duvancho303 <duvancho303@gmail.com>
2022-06-10 19:27:39 -0600

    adding more code

--- 13-DOM/js/02-scripts.js
+++ 13-DOM/js/02-scripts.js
@@ -1,1 +1,7 @@
-// sleccionar elementos por su clase
+// seleccionar elementos por su clase
+// usando get.elementsbyclassname
+
+// seleccionando un elemento por su clase. En este caso "header"
+const header = document.getElementsByClassName('header');
+
+console.log(header);
