27960f106d82912dbb34e56963d3a22229cf007d
duvancho303 <duvancho303@gmail.com>
2022-06-10 19:05:32 -0600

    adding more code

--- 13-DOM/js/01-scripts.js
+++ 13-DOM/js/01-scripts.js
@@ -1,1 +1,19 @@
-// que es el DOM ?
+// Acceder a elementos del HTML con document.
+// el "Document" es todo lo que esta dentro del HTML
+
+let elemento;
+
+elemento = document.all   // todo el html que hay
+elemento = document.head // nos seleciona solo la parde del head
+elemento = document.body // selecciona todo lo que esta dentro de la etiqueta de body
+elemento = document.domain // nos da el ip del hosting de la pagina
+
+
+
+
+
+
+
+
+
+console.log(elemento);
