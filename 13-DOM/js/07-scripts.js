c0844a08b796dae6ac997c33524ccdb276da96c0
duvancho303 <duvancho303@gmail.com>
2022-06-11 07:45:00 -0600

    adding more code

--- 13-DOM/js/07-scripts.js
+++ 13-DOM/js/07-scripts.js
@@ -1,1 +1,21 @@
 // cambiando el CSS con JS
+const encabezado = document.querySelector('h1');
+
+encabezado.style.backgroundColor = 'red'
+encabezado.style.fontSize = '11px';
+encabezado.style.fontFamily = 'arial';
+encabezado.style.textTransform = 'uppercase';
+
+/* en JS los styles como background-color van en una sola palabra usando
+el camel case, no es como en CSS que se tienen que separar por un guion */
+
+
+// añadir una clase a un elemento que ya contiene una clase usando '.add'
+const card = document.querySelector('card');
+card.classList.add('silomonto');
+console.log(card.classList);
+
+//tambien se puede borrar una clase usando ".remove"
+card.classList.remove('.card');
+
+console.log(card.classList);
---
+++ 13-DOM/js/08-scripts.js
@@ -0,0 +1,22 @@
+// traversing el DOM
+// ir recorriendo el DOM...
+
+/*const navegacion = document.querySelector('.navegacion');
+
+console.log(navegacion);
+console.log(navegacion.children);
+// children me va enlistat elementos reales
+console.log(navegacion.childNodes);
+// childnodes me va a enlistar los espacios en blanco
+*/
+
+// vamos a seleccionar algo en especifico dentro de un container 
+const card = document.querySelector('.card');
+
+card.children[1].children[1].textContent = 'Nuevo headings desde el DOM D';
+
+console.log(card.children[1].children[1].textContent);
+
+// aqui vamos a selecionar la imagen dentro del mismo container que el ejemplo de antes
+card.children[0].src = 'img/hacer4.jpg';
+console.log(card.children[0]);
