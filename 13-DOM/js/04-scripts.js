87595f872d045bdceaf7b5ccc6af34e4528418ce
duvancho303 <duvancho303@gmail.com>
2022-06-10 20:31:22 -0600

    adding more code

--- 13-DOM/js/04-scripts.js
+++ 13-DOM/js/04-scripts.js
@@ -1,3 +1,15 @@
 // Usando .querySelector
 // puedes seleciionar classes pero tiene la misma carateristica que
 // getelementbyid... solo te va a devolver el primero que encuentre solo uno
+
+const card = document.querySelector('.card') // usar punto para clase
+
+console.log(card);
+
+// solo nos devuelve el primero que encuentre ! MUY IMPORTANTE!
+// a pesar de que hay mas.....
+
+
+// podemos tener selectores especificos como en CSS
+// igual de como se hace en CSS
+const info = document.querySelector('.premium .info')
