5e705a6d074e3e2a3cb16bea586ac8eadab3b797
duvancho303 <duvancho303@gmail.com>
2022-06-11 21:19:11 -0600

    adding more code

--- 13-DOM/js/09-scripts.js
+++ 13-DOM/js/09-scripts.js
@@ -1,1 +1,13 @@
-// eliminar elementos del DOM
+// eliminar elementos del DOM usando .remove
+const primerEnlace = document.querySelector('a');
+
+primerEnlace.remove();
+
+console.log(primerEnlace);
+
+// eliminar desde el padre
+const navegacion = document.querySelector('.navegacion');
+
+console.log(navegacion.children);
+
+navegacion.removeChild(navegacion.children[2]);
