b2ddf627040ed73f38db950762569a1c5592732f
duvancho303 <duvancho303@gmail.com>
2022-06-11 21:23:22 -0600

    adding more code

--- 13-DOM/js/09-scripts.js
+++ 13-DOM/js/09-scripts.js
@@ -8,6 +8,6 @@ console.log(primerEnlace);
 // eliminar desde el padre
 const navegacion = document.querySelector('.navegacion');
 
-console.log(navegacion.children);
+console.log(navegacion.children); // para saber en que posicion esta lo que quieres borrar
 
 navegacion.removeChild(navegacion.children[2]);
---
+++ 13-DOM/js/10-scripts.js
@@ -0,0 +1,1 @@
+// generar HTML con JS...desde el DOM
