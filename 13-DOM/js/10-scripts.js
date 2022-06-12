00dc262d1975bc7d42c71c24ddb641f3636da3ac
duvancho303 <duvancho303@gmail.com>
2022-06-11 21:38:59 -0600

    adding more code

--- 13-DOM/js/10-scripts.js
+++ 13-DOM/js/10-scripts.js
@@ -1,4 +1,17 @@
 // generar HTML con JS...desde el DOM
 
 // crear un nuevo enlace dentro del navegador de esta pagina
-const enlace = document.cre
+// usando .createElement
+const enlace = document.createElement('a');
+
+//agregandole el texto
+enlace.textContent = 'nuevo enlace';
+
+//agregandole el href
+enlace.href = '/nuevo-enlace';
+
+
+
+
+
+console.log(enlace)
