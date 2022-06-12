4f43661c3ba568a2b908fa451fcca1e9801e0ce1
duvancho303 <duvancho303@gmail.com>
2022-06-11 21:42:10 -0600

    Adding more code

--- 13-DOM/js/10-scripts.js
+++ 13-DOM/js/10-scripts.js
@@ -10,8 +10,12 @@ enlace.textContent = 'nuevo enlace';
 //agregandole el href
 enlace.href = '/nuevo-enlace';
 
-
-
+enlace.target = '_blank';
 
 
 console.log(enlace)
+
+// okay una vez cre todo, donde quiero mostrarlo ?
+const navegacion = document.querySelector('.navegacion');
+
+navegacion.appendChild // se usa ya para unir el texto dentro de .navegacion
