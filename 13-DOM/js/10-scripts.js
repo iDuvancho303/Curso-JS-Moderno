fd948b79d843b27d0cfc320dbe7470e5c8ac1764
duvancho303 <duvancho303@gmail.com>
2022-06-12 21:49:24 -0600

    Adding more code

--- 13-DOM/js/10-scripts.js
+++ 13-DOM/js/10-scripts.js
@@ -12,12 +12,35 @@ enlace.href = '/nuevo-enlace';
 
 enlace.target = '_blank';
 
+enlace.onclick = miFuncion;
+
 
 console.log(enlace)
 
 // okay una vez cre todo, donde quiero mostrarlo ?
 const navegacion = document.querySelector('.navegacion');
 
-navegacion.appendChild // se usa ya para unir el texto dentro de .navegacion
+navegacion.appendChild(enlace); // se usa ya para unir el texto dentro de .navegacion
 
-// que mas?
+// o sino lo puedes poner en un posicion en particular
+navegacion.insertBefore(enlace, navegacion.children[1]);
+// usando insertBefore lo pone dentor de dos elementos en donde tu lo especifiques
+
+function miFuncion() {
+    alert('Diste Click')
+};
+
+
+// otro ejemplo
+// crear un card
+const parrafo1 = document.createElement('p');
+parrafo1.textContent = 'Concierto!';
+parrafo1.classList.add('categoria', 'concierto');
+
+const parrafo2 = document.createElement('p');
+parrafo2.textContent = 'Concierto de Rock';
+parrafo2.classList.add('titulo');
+
+const parrafo3 = document.createElement('p');
+parrafo3.textContent = '$800 por persona';
+parrafo3.classList.add('precio');
