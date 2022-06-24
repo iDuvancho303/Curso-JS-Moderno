ca6f572668dbe1cd099fa856491efd59f7572451
duvancho303 <duvancho303@gmail.com>
2022-06-24 06:14:50 -0600

    adding more code

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -1,5 +1,16 @@
 // Variables
 
 const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos')
 
-const listaCursos = document.querySelector()
+
+/* creando los event listeners para agregar o borrar cursos del carrito
+*/
+
+registrarEventos();
+
+function registrarEventos() {
+    listaCursos.addEventListener('click')
+}
