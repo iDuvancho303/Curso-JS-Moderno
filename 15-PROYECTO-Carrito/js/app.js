bdf6d6ae542debd59040a09b6cfac5770eaa879c
duvancho303 <duvancho303@gmail.com>
2022-06-27 21:12:45 -0600

    Adding more code

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -3,5 +3,6 @@
 const carrito = document.querySelector('#carrito');
 
 const listaCursos = document.querySelector('#lista-cursos');
-
-const 
+// Para selecionar la lista de cursos disponibles dentro del HTML
+const contenedorCarrito = document.querySelector('#lista-carrito tbody'); 
+// Donde van a ir los cursos una vez agregados al carrito
