66e188df2563ef35fcaa67b18eeff3dfb71c5aa9
duvancho303 <duvancho303@gmail.com>
2022-06-30 20:45:31 -0600

    adding more code

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,21 @@
+// Variables
+const btnEnviar = document.querySelector('#enviar');
+
+// Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const  mensaje = document.querySelector('#mensaje');
+eventListeners();
+function eventListeners() {
+    // cuando la app arranca
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    // Campos del formulario
+    email.addEventListener('blur', validarFormulario);
+}
+
+// Funciones
+function iniciarApp() {
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
