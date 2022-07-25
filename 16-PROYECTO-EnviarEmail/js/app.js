b2a23d08637681b62e755063847e9eed507b83e8
duvancho303 <duvancho303@gmail.com>
2022-07-25 14:42:32 -0600

    adding more code

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,7 +1,16 @@
 // Variables
 
+eventListeners();
+
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', iniciarapp);
+}
 
 
 
 
 // Funciones
+
+function iniciarapp() {
+    
+}
