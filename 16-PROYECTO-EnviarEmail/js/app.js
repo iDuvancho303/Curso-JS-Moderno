5bba19388081840717a15126f735ffe27b6345ec
duvancho303 <duvancho303@gmail.com>
2022-07-25 21:57:36 -0600

    adding more code

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -29,9 +29,18 @@ function iniciarApp() {
 
 // Funciones -> validando el formulario
 function validarFormulario(e) {
+
+
     if(e.target.value.length > 0) {
-        console.log("Si hay algo")
+
+    // Elimina los errores
+    const error = document.querySelector('p.error');
+    error.remove();
+
+        e.target.classList.remove('border-red-500');
+        e.target.classList.add('border', 'border-green-500');
     } else {
+        e.target.classList.remove('border-green-500')
         e.target.classList.add('border', 'border-red-500');
 
         mostrarError('Todos lo campos son obligatorios!');
@@ -40,8 +49,13 @@ function validarFormulario(e) {
     if(e.target.type === 'email') {
         const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if(er.test(e.target.value)) {
-            console.log('Email es valido');
+            const error = document.querySelector('p.error');
+    error.remove();
+
+        e.target.classList.remove('border-red-500');
+        e.target.classList.add('border', 'border-green-500');
         } else {
+            e.target.classList.remove('border-green-500')
             e.target.classList.add('border', 'border-red-500');
 
             mostrarError('Email no es correcto');
