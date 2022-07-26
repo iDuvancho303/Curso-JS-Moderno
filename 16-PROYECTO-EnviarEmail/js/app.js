ee8259333102a8df575802ef0c0228efa86891d0
duvancho303 <duvancho303@gmail.com>
2022-07-25 19:08:03 -0600

    adding more code

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,18 +1,20 @@
 // Variables
+const btnEnviar = document.querySelector('#enviar');
 
-const btnEnviar = document.querySelector('#enviar')
-
+// Variables para los 3 campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
 eventListeners();
 
 function eventListeners() {
-    document.addEventListener('DOMContentLoaded', iniciarapp);
+    document.addEventListener('DOMContentLoaded', iniciarApp);
 }
 
 
 
 
 // Funciones
-
-function iniciarapp() {
-    console.log('Iniciandso, texteando y reggaetoneando');
+function iniciarApp() {
+    btnEnviar.disabled = true; 
 }
