a7604f8de66f40c72f178ac65fa84fcfbd06c7e1
duvancho303 <duvancho303@gmail.com>
2022-09-07 21:07:51 -0600

    adding more code

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -36,6 +36,10 @@ function validarFormulario(e) {
 function mostrarError() {
     const mensajeError = document.createElement('p');
     mensajeError.textContent = 'Todos los campos son obligatorios';
-    mensajeError.classList.add('border', 'border-red-500', 'background-color-100', 'text-red-500', 'p-3');
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+    
+    const errores = document.querySelectorAll('.error');
+    if(errores.length === 0) {
     formulario.appendChild(mensajeError);
+    }
 }
