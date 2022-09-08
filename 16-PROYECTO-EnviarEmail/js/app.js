1603749d8afae89063181b86f03a5b8be0165b89
duvancho303 <duvancho303@gmail.com>
2022-09-07 20:41:04 -0600

    adding more code

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,16 @@
+// variables
+const btnEnviar = document.querySelector('#enviar');
+
+// Variables para campos
+const email = document.querySelector
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+}
+//funciones
+function iniciarApp() {
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed');
+}
+
+//
