f17c2965c535f774c14c06236db61c0d80171203
duvancho303 <duvancho303@gmail.com>
2022-09-07 20:53:22 -0600

    adding more code

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -11,12 +11,22 @@ function eventListeners() {
     document.addEventListener('DOMContentLoaded', iniciarApp);
 
     // Campos del formulario
-    email.addEventListener('blur', validarFormulario)
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
+
 }
 //funciones
 function iniciarApp() {
     btnEnviar.disabled = true;
-    btnEnviar.classList.add('cursor-not-allowed');
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
 }
 
-//
+// Valida el formulario
+function validarFormulario(e) {
+    if(e.target.value.length > 1) {
+        console.log('Si Hay Algo Hombre');
+    } else {
+        e.target.classList.add('broder', 'border-red-500');
+    }
+}
