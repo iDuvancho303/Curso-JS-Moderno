d7af8b50e66a325db35861dbf955fba27a900409
Duvan <duvancho303@gmail.com>
2022-10-14 11:00:52 -0600

    adding code

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -6,4 +6,10 @@ const carrito = document.querySelector('#carrito');
 // 2. seleccionando la lista de cursos que hay disponibles
 const listaCursos = document.querySelector('#lista-cursos');
 
+// 3. anadiendo donde van a ir las clases en el carrito despues que se añadan
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+
+// 4. anadiendo / selecionando el btn para vaciar el carrito
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
+
  
