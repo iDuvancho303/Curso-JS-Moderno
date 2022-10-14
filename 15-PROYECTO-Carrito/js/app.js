8d53a6a52456f0fce44a275fbb404592bc3d74e4
Duvan <duvancho303@gmail.com>
2022-10-14 10:53:06 -0600

    adding code

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -1,2 +1,9 @@
 // variables 
 
+// 1. seleccionando el  carrito que esta dentro del navbar
+const carrito = document.querySelector('#carrito');
+
+// 2. seleccionando la lista de cursos que hay disponibles
+const listaCursos = document.querySelector('#lista-cursos');
+
+ 
